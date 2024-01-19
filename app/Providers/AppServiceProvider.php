c1ea01be6473be38f03c191f17868b6a34c6fcc9
Andri Purnama <andri.purnama995@hotmail.com>
2024-01-19 16:37:20 +0700

    feat: added response json format

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Response;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -9,9 +10,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register()
     {
-        //
+        Response::macro('api', function (array $data, int $status) {
+            return response()->json(['data' => $data], $status);
+        });
     }
 
     /**
