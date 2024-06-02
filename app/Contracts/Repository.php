bf295a6a72be55caf0072b082ef82b0a404ac71a
andripurnama <andri.purnama995@hotmail.com>
2024-06-02 23:30:20 +0700

    feat: added repository contract

--- app/Contracts/Repository.php
+++ app/Contracts/Repository.php
@@ -2,10 +2,15 @@
 
 namespace App\Contracts;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
-interface Repository {
-    public function create(array $data) : bool;
-    public function getOne(string $columnName, string $param) : Model;
-    public function delete(string $columnName, $param) : void;
+interface Repository
+{
+    public function create(array $data): bool;
+    public function all(): ?Collection;
+    public function find(int $id);
+    public function update(int $id, array $data): ?Model;
+    public function delete(int $id): bool;
+    public function findBy(string $column, $param): ?Model;
 }
